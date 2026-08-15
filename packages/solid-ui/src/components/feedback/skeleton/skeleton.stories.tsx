38f5871f2d90d1bfb76cdd3dd7670af480ce3339
technohippie <piratesocialclub@proton.me>
2026-08-16 00:01:48 +0400

    fix(solid-ui): size default skeleton story

--- packages/solid-ui/src/components/feedback/skeleton/skeleton.stories.tsx
+++ packages/solid-ui/src/components/feedback/skeleton/skeleton.stories.tsx
@@ -29,7 +29,11 @@ export default meta;
 type Story = StoryObj<typeof meta>;
 
 export const Default: Story = {
-  render: (args) => <Skeleton class={args.class} />,
+  render: (args) => (
+    <div class="w-[calc(100vw-2rem)] max-w-full p-4">
+      <Skeleton class={args.class} />
+    </div>
+  ),
   play: async ({ canvasElement }) => {
     const skeleton = canvasElement.querySelector(".animate-pulse");
     await expect(skeleton).toHaveClass("bg-surface-skeleton");
